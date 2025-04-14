37a62b34d2f3100874403fefb5d9788e6ef3d1df
Dilyara <diliara.kavieva@estudiantat.upc.edu>
2025-04-14 13:12:24 +0200

    Update and rename TImetable.js to Timetable.js

--- models/TImetable.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const StudentSchema = new mongoose.Schema({
-  uid: String,
-  name: String,
-});
-
-module.exports = mongoose.model('Student', StudentSchema);
