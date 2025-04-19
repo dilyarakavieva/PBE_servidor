85b0b1d1ff11b9bdd744220e02df0fc85a35c141
Dilyara <diliara.kavieva@estudiantat.upc.edu>
2025-04-19 14:52:50 +0200

    Update Mark.js

--- models/Mark.js
+++ models/Mark.js
@@ -5,4 +5,4 @@ const MarkSchema = new mongoose.Schema({
   subject_task: String // Para asociar la nota a algo
 });
 
-module.exports = mongoose.model('MRKk', MarkSchema);
+module.exports = mongoose.model('Mark', MarkSchema);
