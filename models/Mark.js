916533e9ae9ab2cb012a45f8349251b440ccb981
Dilyara <diliara.kavieva@estudiantat.upc.edu>
2025-05-17 12:25:26 +0200

    Update Mark.js

--- models/Mark.js
+++ models/Mark.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 
 const MarkSchema = new mongoose.Schema({
-  subject: String,
-  value: Number,
-  student_uid: String
+  uid: { type: String, required: true, index: true },
+  subject: { type: String, required: true },
+  value: { type: Number, required: true },
+  date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Mark', MarkSchema);
