39262e4a62fa856f8871ae4c7b33f1e068af7362
Dilyara <diliara.kavieva@estudiantat.upc.edu>
2025-05-20 15:58:01 +0200

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
-  date: { type: Date, required: true },
-  subject: String,
-  student_uid: String // Para asociar tareas a un estudiante
+  date: { type: Date, required: true, get: v => v.toISOString().split('T')[0]  }, // hemos añadido la fecha
+  subject: String
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
+
+
